5e14c2b0d5ae933053b96a4d5e2447b6536f9b68
peter <peter.berghuis_13@hotmail.com>
2025-07-05 17:25:12 +0900

    add sonar coverage

--- karma.conf.js
+++ karma.conf.js
@@ -3,11 +3,19 @@ module.exports = function (config) {
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     browsers: ['ChromeHeadless'],
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage'),
+      subdir: '.',
+      reporters: [
+        { type: 'lcovonly', subdir: '.', file: 'lcov.info' }
+      ]
+    },
     singleRun: true,
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
+      require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ]
   });
