e20c05c2e6429889b1187ecf977d977ea71b3bd9
peter <peter.berghuis_13@hotmail.com>
2026-02-11 03:06:26 +0900

    angular 19 -> 20

--- src/app/shared/components/loading/loading.component.ts
+++ src/app/shared/components/loading/loading.component.ts
@@ -1,13 +1,11 @@
 import { Component, Input } from '@angular/core';
-import { CommonModule } from "@angular/common";
+
 
 @Component({
   selector: 'app-loading',
   templateUrl: './loading.component.html',
   styleUrls: ['./loading.component.scss'],
-  imports: [
-    CommonModule
-  ]
+  imports: []
 })
 export class LoadingComponent {
   @Input() title: string = 'Loading...';
