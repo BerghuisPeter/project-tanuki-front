44cf7086eae190721c81e44737c53d0282e05e6f
peter <peter.berghuis_13@hotmail.com>
2026-02-12 23:02:03 +0900

    Add theme toggle feature with light and dark mode support in header component

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -4,22 +4,32 @@ import { DomSanitizer } from "@angular/platform-browser";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatButtonModule } from "@angular/material/button";
 import { TitleCasePipe } from "@angular/common";
+import { ThemeService } from "../../services/theme.service";
+import { MatTooltip } from "@angular/material/tooltip";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   standalone: true,
-  imports: [MatIconModule, MatToolbarModule, MatButtonModule, TitleCasePipe],
+  imports: [MatIconModule, MatToolbarModule, MatButtonModule, TitleCasePipe, MatTooltip],
 })
 export class HeaderComponent {
   @Input() title: string = 'headerTitle';
 
-  constructor(private readonly matIconRegistry: MatIconRegistry, private readonly domSanitizer: DomSanitizer) {
+  constructor(private readonly matIconRegistry: MatIconRegistry, private readonly domSanitizer: DomSanitizer, public readonly themeService: ThemeService) {
 
     this.matIconRegistry.addSvgIcon(
       `racoon`,
       this.domSanitizer.bypassSecurityTrustResourceUrl("assets/racoon.svg")
     );
+  }
+
+  get isDarkMode(): boolean {
+    return this.themeService.isDarkMode();
+  }
+
+  get ThemeTooltip(): string {
+    return this.isDarkMode ? 'Light mode' : 'Dark mode';
   }
 }
