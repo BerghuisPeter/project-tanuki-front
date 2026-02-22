4787c11633fa56f581b213fedae929f2e59ee9ef
peter <peter.berghuis_13@hotmail.com>
2026-02-22 23:43:56 +0900

    Improve Google Sign-In UX by disabling the button during loading, refining error handling, and updating the OAuth scope.

--- src/app/shared/components/google-signin/google-signing.component.ts
+++ src/app/shared/components/google-signin/google-signing.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, NgZone, OnInit } from '@angular/core';
+import { Component, inject, NgZone, OnInit, signal } from '@angular/core';
 import { from } from 'rxjs';
 import { environment } from "../../../../environments/environment";
 import { AuthService } from "../../../core/services/auth.service";
@@ -13,7 +13,7 @@ import { APP_PATHS } from "../../models/app-paths.model";
   standalone: true,
   imports: [MatSnackBarModule, MatIconButton],
   template: `
-    <button (click)="onGoogleLoginClick()" type="button" matIconButton>
+    <button (click)="onGoogleLoginClick()" [disabled]="isDisabled()" type="button" matIconButton>
       <svg height="18" viewBox="0 0 48 48" width="18" xmlns="http://www.w3.org/2000/svg">
         <path
           d="M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z"
@@ -37,15 +37,27 @@ export class GoogleSigningComponent implements OnInit {
   private readonly snackBar = inject(MatSnackBar);
   private readonly router = inject(Router);
   private client: google.accounts.oauth2.CodeClient | undefined;
+  protected readonly isDisabled = signal(true);
 
   ngOnInit(): void {
-    from(this.googleScriptService.load()).subscribe(() => {
-      this.initializeGoogle();
+    from(this.googleScriptService.load()).subscribe({
+      next: () => {
+        this.initializeGoogle();
+        this.isDisabled.set(false);
+      },
+      error: (err) => {
+        console.error('Failed to load Google script', err);
+        this.snackBar.open('Failed to load Google Sign-In script. Please contact support.', '', {
+          duration: 5000,
+          panelClass: ['bg-red-500', 'text-white']
+        });
+      }
     });
   }
 
   onGoogleLoginClick() {
     if (this.client) {
+      this.isDisabled.set(true);
       this.client.requestCode();
     }
   }
@@ -53,7 +65,7 @@ export class GoogleSigningComponent implements OnInit {
   private initializeGoogle(): void {
     this.client = google.accounts.oauth2.initCodeClient({
       client_id: environment.googleClientId,
-      scope: 'openid email profile',
+      scope: 'openid email',
       ux_mode: 'popup',
       select_account: true,
       callback: (response: google.accounts.oauth2.CodeResponse) => {
@@ -70,6 +82,7 @@ export class GoogleSigningComponent implements OnInit {
   }
 
   private handleGoogleError(error: google.accounts.oauth2.ClientConfigError): void {
+    this.isDisabled.set(false);
     console.error('Google Auth error:', error);
     let message = 'An error occurred during Google Sign-In.';
 
@@ -88,11 +101,15 @@ export class GoogleSigningComponent implements OnInit {
   }
 
   private handleCredentialResponse(response: google.accounts.oauth2.CodeResponse): void {
-    if (!response.code) return;
+    if (!response.code) {
+      this.isDisabled.set(false);
+      return;
+    }
 
     this.authService.loginWithGoogle(response.code).subscribe({
       next: () => this.router.navigate([APP_PATHS.HOME], { replaceUrl: true }),
       error: (err) => {
+        this.isDisabled.set(false);
         console.error('Google login failed', err);
         this.snackBar.open('Google login failed. Please try again later.', 'Close', {
           duration: 5000,
